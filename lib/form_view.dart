285efd7b23f02c9ad9da55d3a025ac7329327d54
3dsonneto <edson_neto1996@hotmail.com>
2021-05-13 19:36:24 -0300

    Creating the  interface

--- lib/form_view.dart
+++ lib/form_view.dart
@@ -6,8 +6,68 @@ class FormView extends StatefulWidget {
 }
 
 class _FormViewState extends State<FormView> {
+  final _formKey = GlobalKey<FormState>();
+  String name = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.only(right: 18, left: 18),
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.5,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(25)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextFormField(
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return "Campo Nome é obrigatório";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Nome"),
+                    onChanged: (text) {
+                      name = text;
+                    },
+                  ),
+                  TextFormField(
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return "Campo Sobrenome é obrigatório";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Sobrenome"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final isValid = _formKey.currentState.validate();
+                      if (isValid) {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text(name),
+                              );
+                            });
+                      }
+                    },
+                    child: Text("Validar"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
