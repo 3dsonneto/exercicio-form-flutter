fc43cb6e8852fd107f8c4984dc743b8daa023830
3dsonneto <edson_neto1996@hotmail.com>
2021-05-13 19:22:26 -0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/exercicio_form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exercicio_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/form_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FormView extends StatefulWidget {
+  @override
+  _FormViewState createState() => _FormViewState();
+}
+
+class _FormViewState extends State<FormView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
